4ed4b61827ca4497589a38bbc485fd2cec73d7b4
Foo Bar <crowley.13@gmx.de>
2015-05-13 14:14:41 +0200

    added assoziations added model airport

--- multiAssozSameModel/app/models/flight.rb
+++ multiAssozSameModel/app/models/flight.rb
@@ -1,2 +1,5 @@
 class Flight < ActiveRecord::Base
+  belongs_to :departure_airport, class_name: "Airport"
+  belongs_to :arrival_airport, class_name: "Airport" #ActiveRecord erkennt selbst die zugehoerigen _id-Felder
+      # daher kann auf Optionen foreign_key: :departure_airport_id verzichtet werden
 end
