acf2de30f1eb7bc89e112593df45307ac696ccbe
Foo Bar <crowley.13@gmx.de>
2015-05-29 11:38:11 +0200

    testing stuff, title

--- spblog/config/routes.rb
+++ spblog/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'pages/home'
 
   get 'pages/about'
+  
+  get 'pages/directions'
 
   resources :articles do
     resources :comments
--- spblog/test/controllers/pages_controller_test.rb
+++ spblog/test/controllers/pages_controller_test.rb
@@ -4,11 +4,20 @@ class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    # pruefe ob Seite einen Titel namens Welcome hat
+    assert_select "title", "Welcome"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About us"
+  end
+
+  test "should get directions" do
+    get :directions
+    assert_response :success
+    assert_select "title", "Directions"
   end
 
 end
