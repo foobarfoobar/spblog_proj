a118650245a2c7c35739f32622fb7940412904a3
Foo Bar <crowley.13@gmx.de>
2015-05-29 12:23:32 +0200

    added helpers and style

--- spblog/app/helpers/application_helper.rb
+++ spblog/app/helpers/application_helper.rb
@@ -1,2 +1,47 @@
 module ApplicationHelper
+  
+  def get_season
+    time = Time.new
+    if ( time.month >= 3 && time.month <= 5 )
+      "Yeah its Spring"
+    elsif ( time.month > 5 && time.month <= 8 )
+      "Summertime!"
+    elsif ( time.month > 8 && time.month <= 10 )
+      "Autumn - awesome"
+    else
+      "Wintertime"
+    end
+  end
+
+  # in Ruby, no return is needed
+  def get_random_number(max_value = 10)
+    rand(max_value)
+  end
+
+  def get_random_welcome
+    opening = ["Hallo Dude.",
+      "Welcome to my site!",
+      "Whats up?"]
+    middle = ["Hopefully you find what you need.",
+      "Here's a wide selection.",
+      "Looking for something?"]
+    ending = ["Contact for help!",
+      "Leave a message.",
+      "Go away!"]
+      
+      "#{opening[rand(3)]} #{middle[rand(3)]} #{ending[rand(3)]}"
+  end
+  
+  def count_to_ten
+    x = 2
+    number_list = "1"
+    
+    loop do
+      number_list = number_list + ", " + x.to_s
+      x += 1
+      break if x > 10
+    end
+    "#{number_list}"
+  end
+
 end
